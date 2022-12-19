1f4e09d451884f82da402f30b4a48a590c37353c
Guseva <nastyarz@gmail.com>
2022-12-19 11:39:52 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 100;
+        int deposit = 1500;
+        int bonus;
+        if (deposit >= 1000) {
+            bonus = deposit / 100;
+        } else {
+            bonus = 0;
+        }
+        int result = account + deposit + bonus;
+
+        System.out.println(result);
+    }
+}
