acdbf48ff556908bc55d99f679bebb46e13c0310
Guseva <nastyarz@gmail.com>
2022-12-22 08:29:13 +0400

    Update Main

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,14 @@ public class Main {
         int account = 100;
         int deposit = 1500;
         int bonus;
-        if (deposit >= 1000) {
+        if (deposit >= 1001) {
             bonus = deposit / 100;
         } else {
             bonus = 0;
         }
+
+        //int bonus = deposit >= 1001 ? deposit / 100 : 0;
+
         int result = account + deposit + bonus;
 
         System.out.println(result);
